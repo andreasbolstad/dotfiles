d78abb0e5fc16d23ad4ade2036ea4d7b8792c0b5
Andreas Bolstad <26123972+andreasbolstad@users.noreply.github.com>
2023-12-31 10:23:24 +0100

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
+config.font = wezterm.font 'JetBrains Mono'
 config.color_scheme = "Catppuccin Mocha"
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "NONE"
 return config
